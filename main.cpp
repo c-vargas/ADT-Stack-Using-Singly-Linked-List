ed467cc0ca6a0ccfdb7b5a7174da3d4c6936c4f4
Christian Vargas <74288645+c-vargas@users.noreply.github.com>
2020-12-22 17:37:57 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 Written By: Christian Vargas
 */
 
-// This program implements basic stack ADT operations using a singly linked list. This functions in a similar manner as if you were to make a stack of plates. In this scenario, adding or removing is only possible at the top.
+// This program implements basic stack ADT operations using a singly linked list.
+// This functions in a similar manner as if you were to make a stack of plates. In this scenario, adding or removing is only possible at the top.
 
 // main file
 #include <iostream>
@@ -11,7 +12,9 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-// The main() function here gives the user a choice on whether they want to PUSH, POP, or DISPLAY the stack. The proper fucntion is called here using switch. Similarly, if the user enters an invalid choice, then it is made known to the user. There is also an option to EXIT the progam. 
+// The main() function here gives the user a choice on whether they want to PUSH, POP, or DISPLAY the stack. 
+// The proper fucntion is called here using switch. Similarly, if the user enters an invalid choice, then it is made known to the user.
+// There is also an option to EXIT the progam. 
 
 int main(){
 
